72af502c0c93da31c1c91fb8a26a67bfeee1f8c9
Eduardo Barros <joseeduardobarros@gmail.com>
2020-03-23 03:32:16 -0300

    Configure vuefire and firebase

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+import firebaseConfig from './firebaseConfig'
+
+// Firebase App
+export default firebase.initializeApp(firebaseConfig)
+
+// Firestore
+export const database = firebase.firestore()
+export const categoriesRef = database.collection('categories')
+
+// Firestore Methods
+const { Timestamp, GeoPoint, FieldValue } = firebase.firestore
+export { Timestamp, GeoPoint, FieldValue }
---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyAzKCSRUAEv2MmpUQko2fSLzxFCxlGLl-A',
+  authDomain: 'opiniao-com-br.firebaseapp.com',
+  databaseURL: 'https://opiniao-com-br.firebaseio.com',
+  projectId: 'opiniao-com-br',
+  storageBucket: 'opiniao-com-br.appspot.com',
+  messagingSenderId: '747737326209',
+  appId: '1:747737326209:web:711bb228ad7a1e6caf2380',
+  measurementId: 'G-5835DRXF21'
+}
