107a19c592dc5ad9c8cdf78f7e23ab60be664a11
Eduardo Barros <joseeduardobarros@gmail.com>
2020-03-23 03:29:27 -0300

    Create CategoryList component

---
+++ admin/components/CategoryList.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-btn color="success" @click="create">Criar Categoria</v-btn>
+    <v-row dense>
+      <category-list-item
+        v-for="category in categories"
+        :key="category.id"
+        :category-id="category.id"
+      ></category-list-item>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import CategoryListItem from '~/components/CategoryListItem.vue'
+export default {
+  components: {
+    CategoryListItem
+  },
+  computed: {
+    categories() {
+      return this.$store.getters['categories/getCategoryList']
+    }
+  },
+  methods: {
+    create() {
+      this.$store.dispatch('categories/create')
+    }
+  }
+}
+</script>
+
+<style></style>
