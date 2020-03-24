97dfecd1a0431c8ab87a77f8df7a32c53d51e32e
Eduardo Barros <joseeduardobarros@gmail.com>
2020-03-24 05:35:13 -0300

    Clean layout design; Add middleware

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,25 +18,37 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar fixed app>
+    <v-app-bar fixed app elevate-on-scroll color="white">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>{{ icons.mdiShareVariant }}</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
+      <nuxt />
     </v-content>
-    <v-footer app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer
+      app
+      absolute
+      color="white"
+      class="text-center d-flex justify-center grey--text"
+      elevation="2"
+    >
+      <span>Lanchoente Opinião &copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import { mdiShareVariant } from '@mdi/js'
 export default {
   data() {
     return {
+      icons: {
+        mdiShareVariant
+      },
       drawer: false,
       items: [
         {
@@ -50,8 +62,9 @@ export default {
           to: '/inspire'
         }
       ],
-      title: 'Lanchoente Opinião'
+      title: 'Lanchonete Opinião'
     }
-  }
+  },
+  middleware: 'database'
 }
 </script>
