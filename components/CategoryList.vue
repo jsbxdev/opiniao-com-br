d688771b7da15b0227352cf67224a8a95df3af60
Eduardo Barros <joseeduardobarros@gmail.com>
2020-03-24 05:34:24 -0300

    Create CategoryList component

--- components/CategoryList.vue
+++ components/CategoryList.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <v-container class="pa-0">
+    <category-list-item
+      v-for="(category, index) in categories"
+      :key="category.id"
+      :category-id="category.id"
+      :class="{
+        'first-category': index === 0,
+        'last-category': index === categories.length - 1
+      }"
+    ></category-list-item>
+  </v-container>
 </template>
 
 <script>
-export default {}
+import CategoryListItem from './CategoryListItem.vue'
+export default {
+  components: {
+    CategoryListItem
+  },
+  computed: {
+    categories() {
+      return this.$store.getters['categories/getCategoryList']
+    }
+  }
+}
 </script>
 
 <style></style>
