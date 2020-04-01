3fe3037dc4f7dfb3cff4f82dc5bbe57152a0f533
Eduardo Barros <joseeduardobarros@gmail.com>
2020-04-01 07:51:09 -0300

    Add About component and WhatsApp link

---
+++ app/components/About.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card elevation="0">
+    <v-container class="px-6">
+      <v-row no-gutters>
+        <v-col cols="12">
+          <h1 class="title font-weight-black">Lanchonete Opinião</h1>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col cols="12">
+          <h1 class="body-2">Palmital - SP</h1>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-card link @click="openWhatsApp">
+            <v-list-item two-line>
+              <v-list-item-avatar>
+                <v-icon size="26">{{ icons.mdiMoped }}</v-icon>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="body-2 font-weight-medium">
+                  Disk Entrega
+                </v-list-item-title>
+                <v-list-item-title class="body-2">
+                  WhatsApp:
+                  <span>018 3351 3545</span>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+import { mdiMoped, mdiWhatsapp } from '@mdi/js'
+export default {
+  data() {
+    return {
+      icons: {
+        mdiMoped,
+        mdiWhatsapp
+      }
+    }
+  },
+  methods: {
+    openWhatsApp() {
+      window.open('https://wa.me/551833513545', '_blank')
+    }
+  }
+}
+</script>
+
+<style></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <v-container class="pa-0">
+    <About></About>
     <CategoryList></CategoryList>
   </v-container>
 </template>
 
 <script>
+import About from '~/components/About.vue'
 import CategoryList from '~/components/CategoryList.vue'
 export default {
   components: {
+    About,
     CategoryList
   }
 }
